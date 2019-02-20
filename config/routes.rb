f9f1882ed6b498ba71a6b18436361bb6f9d7e4eb
nitkna <nithin.krishna@outlook.com>
2019-02-20 14:26:15 +0530

    sample app

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/contact'
   get 'pages/about'
+  get 'pages/help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
