40eebc4cf89b7e61003b950abf5662a811329296
nitkna <nithin.krishna@outlook.com>
2019-02-21 20:09:30 +0530

    Finished first cut of User Model

--- Gemfile
+++ Gemfile
@@ -55,11 +55,8 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-group :development do
-  gem 'rspec-rails', '2.0.1'
+group :development, :test do
+  gem 'rspec-rails', '3.8'
+  
 end
 
-group :test do
-  gem 'rspec', '2.0.1'
-  gem 'webrat', '0.7.1'
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  
 
   get '/' => "pages#home", as: :home
   get 'pages/help' => "pages#help", as: :help
   get 'pages/contact' => "pages#contact", as: :contact
   get 'pages/about' => "pages#about", as: :about
-  get 'users/new' => "users#new", as: :new
+  get '/new' => "users#new", as: :new
+  resources :users
   
   
 end
