5d5ce76da61ec7138ebc0127dc5140eb1b87ac43
nitkna <nithin.krishna@outlook.com>
2019-02-25 12:44:01 +0530

    done wit user edit/update index and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
 
+  before_action :authenticate , :only => [:edit, :update] #only signed in user can edit  profile
+  before_action :correct_user, :only => [:edit, :update] #users should not edit others profile
+  before_action :admin_user, :only => :destroy
+
+  def index
+    @title = "All Users"
+    @users = User.paginate(:page =>  params[:page])
+  end
+  
   def show
     @user = User.find(params[:id])
     @title =@user.name
@@ -8,6 +17,27 @@ class UsersController < ApplicationController
   def new
     @user = User.new
     @title = "SignUp"
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit User"
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User Deleted"
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user].to_unsafe_hash) #protected by forbidden attributes
+      flash[:success] = "Profile Updated"
+      redirect_to @user
+    else
+      @title = "Edit User"
+      render 'edit'
+    end
   end
 
   def create
@@ -25,6 +55,23 @@ class UsersController < ApplicationController
   private
   def allowed_params
     params.require(:user).permit(:name, :email, :password)
+  end
+
+  def authenticate
+    deny_access unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+
+  def admin_user
+    if not current_user.admin?
+      redirect_to(users_path)
+    else
+      true
+    end
   end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_22_080524) do
+ActiveRecord::Schema.define(version: 2019_02_25_064644) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_02_22_080524) do
     t.datetime "updated_at", null: false
     t.string "encrypted_password"
     t.string "salt"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
